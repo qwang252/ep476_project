2cf856c6f8dd2b81dbe80bcacb7c2379019b8de8
qingquan <qwang252@wisc.edu>
2016-05-10 19:14:30 -0500

    add single panel method

--- flat_panel_velocity.py
+++ flat_panel_velocity.py
@@ -5,7 +5,7 @@
 # from pos1 to pos2 of strength gamma_1
 import numpy as np
 import math
-def flat_sourece_panel_velocity(pos1,pos2,dest_pos,lambda_1): 
+def source_panel_velocity(pos1,pos2,dest_pos,lambda_1): 
     pos1=np.array([pos1[0],pos1[1],0])
     pos2=np.array([pos2[0],pos2[1],0])
     dest_pos=np.array([dest_pos[0],dest_pos[1],0])
@@ -24,7 +24,8 @@ def flat_sourece_panel_velocity(pos1,pos2,dest_pos,lambda_1):
     
     source_velocity=lambda_1/2/math.pi*(np.log(r1_mag/r2_mag)*t_hat+alpha*n_hat)
     return source_velocity 
-def flat_vortex_panel_velocity(pos1,pos2,dest_pos,gamma_1):
+
+def vortex_panel_velocity(pos1,pos2,dest_pos,gamma_1):
     pos1=np.array([pos1[0],pos1[1],0])
     pos2=np.array([pos2[0],pos2[1],0])
     dest_pos=np.array([dest_pos[0],dest_pos[1],0])
@@ -39,8 +40,7 @@ def flat_vortex_panel_velocity(pos1,pos2,dest_pos,gamma_1):
     r1r2_cross=np.cross(r1,r2)
     alpha=math.atan2(np.dot(r1r2_cross,z_hat),np.dot(r1,r2))
     n_hat=np.cross(z_hat,t_hat)
-    
-    vortex_velocity = gamma_1/2/math.pi*(-n_hat*np.log(r1_mag/r2_mag)+t_hat*alpha)
+
+
+    vortex_velocity=gamma_1/2/math.pi*(-np.log(r1_mag/r2_mag)*n_hat+alpha*t_hat)
     return vortex_velocity
-
-
