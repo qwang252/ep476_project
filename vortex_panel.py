769bc25c9a6cf6c9ca5de507d1c1284a86bc63bf
qingquan <qwang252@wisc.edu>
2016-05-20 10:47:01 -0500

    delete truncation_plotter

--- vortex_panel.py
+++ vortex_panel.py
@@ -39,27 +39,7 @@ def vortex_panel(af,rho_infinity,P_infinity,v_infinity,angle_attack):
     Cl = LIFT/rho_infinity/v_infinity**2*2
     Cd = DRAG/rho_infinity/v_infinity**2/A_af*2/mean_y
     return Cl,Cd  
-    
-def truncation_plotter(af):
 
-    indices = af.indices_generate()
-
-    n=len(indices)
-    indices = np.array([[indices[i,0],indices[i,1],0] for i in range(0,n)])
-
-    #define normal and tangential directions for each panel
-    indices_2 = np.concatenate((indices,np.array([indices[0]])),axis=0)
-
-    length_panel = np.linalg.norm(np.diff(indices_2,axis=0),axis=1)
-    t_hat = np.dot(np.diag(1/length_panel),np.diff(indices_2,axis=0))
-
-    z_hat = np.array([0,0,1])
-    z_hat = np.tile(z_hat,(n,1))
-
-    n_hat = np.cross(z_hat,t_hat)
-    #define midpoints for each panel
-    midpt = af.midpts(indices)
-    return indices,midpt,n_hat,t_hat,length_panel
 def normal_velocity(indices,midpt,n_hat):
     n=len(indices)
     L=[]
